335320631ea07de465ef50e3c8c979601eb13db6
mdop297 <lenhatminh297@gmail.com>
2025-04-27 16:12:11 +0700

    chore(setup): update lint staged

--- apps/auth-svc/src/main.ts
+++ apps/auth-svc/src/main.ts
@@ -19,10 +19,3 @@ const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
 server.on('error', console.error);
-// dummy code to test commit hook.
-const message = 'hello';
-function logMessage(msg: string) {
-  console.log(msg);
-}
-
-logMessage(message);
