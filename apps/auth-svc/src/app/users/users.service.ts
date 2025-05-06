8b77a24d07cd6903bcff44fc76a7cdd14f6ae9fd
mdop297 <lenhatminh297@gmail.com>
2025-05-06 10:13:15 +0700

    fix(create user): exception handler, add validation pipe for user

--- apps/auth-svc/src/app/users/users.service.ts
+++ apps/auth-svc/src/app/users/users.service.ts
@@ -1,9 +1,14 @@
-import { Body, Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import {
+  Body,
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from '../prisma/prisma.service';
 import { compareSync, genSaltSync, hashSync } from 'bcryptjs';
-import { User, Prisma } from '@prisma/client';
+import { User, Prisma } from '@prisma-client/user';
+import { PrismaClientKnownRequestError } from '@prisma-client/user/runtime/library';
 
 @Injectable()
 export class UsersService {
@@ -19,17 +24,35 @@ export class UsersService {
     return compareSync(password, hash);
   }
 
-  async create(createUserDto: CreateUserDto): Promise<User> {
-    const hashPassword = this.getHashPassword(createUserDto.password);
-    const result = await this.prisma.user.create({
-      data: {
-        email: createUserDto.email,
-        password: hashPassword,
-        name: createUserDto.name,
-      },
-    });
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    try {
+      const result = await this.prisma.user.create({
+        data: {
+          ...data,
+          password: this.getHashPassword(data.password),
+          name: data.email.split('@')[0],
+        },
+      });
 
-    return result;
+      return result;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          const target = error?.meta?.target as string[];
+          if (target?.includes('email')) {
+            throw new ConflictException('Email already exists');
+          } else if (target?.includes('username')) {
+            throw new ConflictException('Username already exists');
+          } else {
+            throw new ConflictException('A unique constraint error occurred');
+          }
+        }
+      } else {
+        throw new InternalServerErrorException(
+          'Authentication Service Error. Can not create user.',
+        );
+      }
+    }
   }
 
   findAll() {
