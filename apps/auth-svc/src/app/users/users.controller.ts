67d73295f1cffcd6e7644d8d6eebf1ff37f2622a
mdop297 <lenhatminh297@gmail.com>
2025-05-07 10:05:40 +0700

    build(auth): rewrite Dockerfile for auth-svc

--- apps/auth-svc/src/app/users/users.controller.ts
+++ apps/auth-svc/src/app/users/users.controller.ts
@@ -8,15 +8,18 @@ import {
   Delete,
   NotFoundException,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Public } from '../decorators/customize';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Public()
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
