7ca0ef7942a705a43433a0f7f3dce9aca651a36d
mdop297 <lenhatminh297@gmail.com>
2025-10-20 08:58:49 +0700

    refactor(notification-svc): improve event processing logic by extracting value and type retrieval

--- apps/notification-svc/src/notify/events/handlers/event_manager.py
+++ apps/notification-svc/src/notify/events/handlers/event_manager.py
@@ -13,31 +13,18 @@ class EventManager:
         self.service_name = service_name
 
     def register_handler(self, handler: EventHandler) -> None:
-        """Register a single event handler"""
+        """Register a single event handler by event type saved in itself"""
         self._handlers[handler.get_event_type()] = handler
 
     def register_handlers(self, handlers: list[EventHandler]) -> None:
-        """Register multiple handlers"""
+        """Register multiple handlers by event type"""
         for handler in handlers:
             self.register_handler(handler)
 
     async def process_event(self, message: Message) -> bool:
         """Route event to appropriate handler by event type"""
-        value = message.value()
-        if value is None:
-            logger.error(
-                "Received message with no value (None). Skipping event processing."
-            )
-            return False
-        headers = dict(message.headers() or [])
-        event_type_header = headers.get("event-type")
-        if event_type_header is None:
-            logger.error(
-                "Received message with no event type in headers. Skipping event processing."
-            )
-            return False
-
-        event_type = EventType(event_type_header.decode())
+        value = self.get_message_value(message)
+        event_type = self.get_event_type(message)
         handler = self._handlers.get(event_type)
         if handler:
             schema = handler.get_event_schema()
@@ -45,3 +32,31 @@ class EventManager:
             return await handler.handle(event)
 
         return False
+
+    def get_event_type(self, message: Message) -> EventType:
+        headers = dict(message.headers() or [])
+        event_type_header = headers.get("event-type")
+        if event_type_header is None:
+            logger.error(
+                "Received message with no event type in headers."
+                + "Skipping event processing."
+            )
+            raise Exception(
+                "Received message with no event type in headers"
+                + "Skipping event processing."
+            )
+        return EventType(event_type_header.decode())
+
+    def get_message_value(self, message: Message):
+        value = message.value()
+        if value is None:
+            logger.error(
+                "Received message with no value (None). Skipping event processing."
+            )
+            raise Exception(
+                "Received message with no value (None)." + "Skipping event processing."
+            )
+        return value
+
+    def send_to_dlq(self, message: Message) -> None:
+        pass
