186b4ea37c6c8661da9ed43fe6d6d0cbb06a176d
mdop297 <lenhatminh297@gmail.com>
2025-11-04 22:58:08 +0700

    feat(core): note, add create, update, response schema to handle request

---
+++ apps/documents/src/schemas/requests/note.py
@@ -0,0 +1,20 @@
+from typing import Optional
+from uuid import UUID
+from datetime import datetime
+from pydantic import BaseModel
+
+
+class NoteCreate(BaseModel):
+    user_id: UUID
+    parent_id: Optional[UUID] = None
+    title: str
+    icon: Optional[str] = None
+    favorite: bool = False
+
+
+class NoteUpdate(BaseModel):
+    parent_id: Optional[UUID] = None
+    title: Optional[str] = None
+    icon: Optional[str] = None
+    favorite: Optional[bool] = None
+    deleted_at: Optional[datetime] = None
