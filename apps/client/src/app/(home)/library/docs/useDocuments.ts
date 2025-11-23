1e6c612eb262080218a8fe4ba6d8eb88a40132e9
mdop297 <lenhatminh297@gmail.com>
2025-11-23 22:57:31 +0700

    feat(client): add feat to update name of document

---
+++ apps/client/src/app/(home)/library/docs/useDocuments.ts
@@ -0,0 +1,15 @@
+import { coreApi } from '@/lib/api';
+import { useQuery } from '@tanstack/react-query';
+
+export const useDocuments = () =>
+  useQuery({
+    queryKey: ['docs'],
+    queryFn: async () => {
+      const res = await coreApi.getDocumentsByFolderId();
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+      return res.data;
+    },
+    staleTime: 60_000 * 15,
+  });
