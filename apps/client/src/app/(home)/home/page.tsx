c970a83eb3b2c3d54a818ebbf74eddf8946f588d
mdop297 <lenhatminh297@gmail.com>
2025-11-28 12:59:01 +0700

    chore(client): add ai-elements library from vercel

--- apps/client/src/app/(home)/home/page.tsx
+++ apps/client/src/app/(home)/home/page.tsx
@@ -1,13 +1,17 @@
-import { ChatInput } from '@/components/chat/ChatInput';
+// import { ChatInput } from '@/components/chat/ChatInput';
+
+import ChatInput from '@/components/chat/PromptInput';
 import React from 'react';
 
 const Chats = () => {
   return (
-    <div className="flex flex-col gap-10 justify-center items-center h-full px-8 mx-auto ">
-      <div className="text-3xl font-sans">
-        <p>What is on the agenda today?</p>
+    <div className="flex justify-center items-center h-full">
+      <div className="flex flex-col gap-10 justify-center items-center px-8 w-3xl mx-auto">
+        <div className="text-3xl font-sans">
+          <p>Let&apos;s get started?</p>
+        </div>
+        <ChatInput />
       </div>
-      <ChatInput />
     </div>
   );
 };
